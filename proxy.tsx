581f9a9ac30db183f6b420bddd8bbbd88b709eb3
bobulousness <bobulousness@gmail.com>
2026-03-05 12:45:47 -0500

    documentation pt2

--- proxy.tsx
+++ proxy.tsx
@@ -3,5 +3,5 @@ import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 export default authkitMiddleware();
 
 // Match against pages that require authentication
-// Leave this out if you want authentication on every page in your application
+// update this for each route that you plan on a asking for the user on
 export const config = { matcher: ['/','/character/select', '/character'] };
